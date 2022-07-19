af5461503d9412aadefd0d6849ee6bf424c30e62
Haader <37Hader38@gmail.com>
2022-07-18 21:30:51 -0300

    chan

--- logica/inicio.js
+++ logica/inicio.js
@@ -1,7 +1,23 @@
 
 myLista.forEach((element,index)=>{
 
-    document.getElementById("ContainerInicio").innerHTML+='<div class="divcard1 "><figure ><img class="cardimg1" id="imgdiv2" src="./IMAGENES/inicio/inicio ('+myLista[index].id+').jpeg" alt="" > </figure><p class="card-text ">'+myLista[index].articulo+'</p>  <a href="'+linkInicio+'" class="btn btn-primary bg-gradient"> Ver</a> </div>';
+    document.getElementById("ContainerInicio").innerHTML+=`
+    
+
+    <div class="divcard1 ">
+    
+        <figure >
+            <img class="cardimg1" id="imgdiv2" src="./IMAGENES/inicio/inicio (${myLista[index].id}).jpeg" alt="" >
+        </figure>
+
+            <p class="card-text">${myLista[index].articulo}</p> 
+
+            <a href="${linkInicio}" class="btn btn-primary bg-gradient"> Ver</a>
+
+    </div>
+
+    `;
+    
     console.log("cargando inicio "+element);
 });
 
